40f781bceeb5806029a7bff5c440a89e6ca5945e
SSvitlana82 <144372279+SSvitlana82@users.noreply.github.com>
2024-04-09 09:55:50 +0300

    modul 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
-/* import { useState } from "react"; */
+import { useState } from "react";
 import Task2 from "./components/Task2";
 import Task3 from "./components/Task3";
 import Task4 from "./components/Task4";
 import Task5 from "./components/Task5";
 import Task6 from "./components/Task6";
+import Task7 from "./components/Task7";
+import LoginForm from "./components/LoginForm";
+import SearchBar from "./components/SearchBar";
+import Coffee from "./components/Coffee";
+import Checkform from "./components/Checkform";
+import FormLogin from "./components/FormLogin";
 
 import "./App.css";
 
 const App = () => {
+  /* const handleLogin = (userData) => {
+    console.log(userData);
+  };
+  const [langValue, setLangValue] = useState("uk"); */
   return (
     <>
       {/* <Task2 />
       <Task3 />
-  <Task4 /> */}
+      <Task4 />
       <Task5 />
-      <Task6 />
+      <Task6 /> 
+      <Task7 />
+      <LoginForm onLogin={handleLogin} />
+      <SearchBar langValue={langValue} onSelect={setLangValue} />
+      <p>{langValue}</p>*/}
+      <Coffee />
+      <Checkform />
+      <FormLogin />
     </>
   );
 };
